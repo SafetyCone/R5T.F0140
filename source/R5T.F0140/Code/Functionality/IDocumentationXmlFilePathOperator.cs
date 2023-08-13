d379ad47890c72bbb613a57a303d98fb1b92480e
davidcoats <david@getsafetycone.com>
2023-08-12 23:22:33 -0700

    Interim changes.

--- source/R5T.F0140/Code/Functionality/IDocumentationXmlFilePathOperator.cs
+++ source/R5T.F0140/Code/Functionality/IDocumentationXmlFilePathOperator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 
 using R5T.T0132;
+using R5T.T0159;
 using R5T.T0172;
 using R5T.T0172.Extensions;
 using R5T.T0180;
@@ -19,8 +20,13 @@ namespace R5T.F0140
         /// Given a directory path, get all child XML file paths, and assume that all XML files are documentation files.
         /// </summary>
         public IDocumentationXmlFilePath[] Get_DocumentationXmlFilePaths_AssumeAllXmls(
-            IDirectoryPath directoryPath)
+            IDirectoryPath directoryPath,
+            ITextOutput textOutput)
         {
+            textOutput.WriteInformation("Getting documentation file paths in:\n\t{0}",
+                directoryPath);
+            textOutput.WriteInformation("=> Assume all XML files are documentation files.");
+
             // Assumes that all XML files in the directory are documentation files.
             var output = Instances.FileSystemOperator.Enumerate_ChildFilePaths(
                 directoryPath,
@@ -28,7 +34,20 @@ namespace R5T.F0140
                 .Select(x => x.Value.ToDocumentationXmlFilePath())
                 .ToArray();
 
+            textOutput.WriteInformation("{0} documentation files found.",
+                output.Length);
+
             return output;
+        }
+
+        public IDocumentationXmlFilePath[] Get_DocumentationXmlFilePaths_AssumeAllXmls(
+            IDirectoryPath directoryPath)
+        {
+            var textOutput = Instances.TextOutputOperator.Get_New_Null();
+
+            return this.Get_DocumentationXmlFilePaths_AssumeAllXmls(
+                directoryPath,
+                textOutput);
         }
     }
 }
--- source/R5T.F0140/Code/Instances.cs
+++ source/R5T.F0140/Code/Instances.cs
@@ -7,5 +7,6 @@ namespace R5T.F0140
     {
         public static Z0044.Z000.IFileExtensions FileExtensions => Z0044.Z000.FileExtensions.Instance;
         public static L0035.IFileSystemOperator FileSystemOperator => L0035.FileSystemOperator.Instance;
+        public static T0159.ITextOutputOperator TextOutputOperator => T0159.TextOutputOperator.Instance;
     }
 }
